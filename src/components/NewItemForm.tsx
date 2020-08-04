167c94e23baf18f727afb3a08d49a041a69bbf13
Mouhamed Mrad <mouhamed.mrad@oyez.fr>
2020-08-04 16:59:12 +0100

    automatically focus on input using refs

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react"
+import { useFocus } from "../utils/useFocus";
+
 import { NewItemFormContainer, NewItemButton, NewItemInput} from "../styles";
 
 interface NewItemFormProps {
     onAdd(text: string): void
 }
 
-export const NewItemForm = (props: NewItemFormProps) => {
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState("")
-    const { onAdd } = props;
+    const inputRef = useFocus()
 
     return (
         <NewItemFormContainer>
             <NewItemInput
+                ref={inputRef}
                 value={text}
                 onChange={e => setText(e.target.value)}
             />
