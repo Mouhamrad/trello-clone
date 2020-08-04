181f675a1c002ee893497f1792e74e66b5a11d08
Mouhamed Mrad <mouhamed.mrad@oyez.fr>
2020-08-04 16:44:10 +0100

    Components For Adding New Items and new list

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import {Column} from "./components/Column";
-import {Card} from "./components/Card";
+import { Column } from "./components/Column";
+import { Card } from "./components/Card";
+import { AddNewItem } from "./components/AddNewItem";
 
-import { AppContainer} from "./styles";
+import { AppContainer } from "./styles";
 
 function App() {
   return (
-<AppContainer>
-  <Column text={"TO DO"}>
-    <Card text="Generate app scaffold" />
-  </Column>
-  <Column text="In Progress">
-    <Card text="Learn Typescript"/></Column>
-  <Column text="Done">
-  <Card text="Begin to use static typing"/></Column>
-</AppContainer>
+      <AppContainer>
+        <Column text="TO DO">
+          <Card text="Generate app scaffold" />
+        </Column>
+        <Column text="In Progress">
+          <Card text="Learn Typescript"/>
+        </Column>
+        <Column text="Done">
+          <Card text="Begin to use static typing"/>
+        </Column>
+        <AddNewItem onAdd={console.log} toggleButtonText="+ Add another list" />
+      </AppContainer>
   );
 }
 
